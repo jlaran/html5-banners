b7cdd083733dc2c51e7aec3eaa97e2680f9a0d01
Juan Lara Nassar <jlaranas@crpgp----------.global.publicisgroupe.net>
2015-04-17 14:07:34 -0600

    final v2

--- mothersday/Mothers Day Samsung Galaxy S6/js/script.js
+++ mothersday/Mothers Day Samsung Galaxy S6/js/script.js
@@ -20,9 +20,9 @@ function expanded() {
     Enabler.requestExpand();
     motionLibrary.animations("fadeIn", "#expanded-banner", 0)
     motionLibrary.animations("fadeIn", "#expanded-banner #info", 0)
-    TweenMax.from("#device1", 3, {top:"-500px", ease:Strong.easeOut})
-    TweenMax.from("#shadow", 3, {top:"-50px", ease:Strong.easeOut})
-    TweenMax.to("#expanded-banner #info #f1_txt1", 2, { opacity:1, onComplete:frame2})
+    TweenMax.from("#device1", 3, {top:"-500px", ease:Strong.easeOut, delay:.5})
+    TweenMax.from("#shadow", 3, {top:"-50px", ease:Strong.easeOut, delay:.5})
+    TweenMax.to("#expanded-banner #info #f1_txt1", 2, { opacity:1, delay:.5, onComplete:frame2})
 }
 
 function frame2() {
